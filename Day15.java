cd05ac22418898f1b8aa6ad8040a13fdbaf3576e
Shivang <shivang.sharma15@outlook.com>
2020-07-09 15:45:41 +0530

    updated day-15 code

--- Day15.java
+++ Day15.java
@@ -30,3 +30,23 @@ class Solution {
     }
 
 	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
